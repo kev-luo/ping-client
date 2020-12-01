c6275cda968265297d678cf5972737fdc846fa66
Kevin Luo <kvn.luo@gmail.com>
2020-12-01 17:59:15 -0500

    replace loading text with loading bar

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { LinearProgress, CircularProgress } from '@material-ui/core';
 
-export default function Loading() {
-  return <div>Loading</div>;
+export default function Loading(comp) {
+  const classes = useStyles()
+  const loadingType = comp === "profile" ? (
+    <CircularProgress />
+  ) : comp === "map" ? (
+    <CircularProgress />
+  ) : (
+    <LinearProgress />
+  )
+
+  return (
+    <div className={classes.container}>
+      {loadingType}
+    </div>
+  )
 }
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    width: "100%",
+    "& > *": {
+      marginTop: theme.spacing(2)
+    }
+  }
+}))
