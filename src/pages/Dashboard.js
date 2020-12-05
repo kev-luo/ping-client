1e67a2019a523885e56835c317af149b466dfd26
Kevin Luo <kvn.luo@gmail.com>
2020-12-05 16:55:36 -0500

    clean up single ping page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -35,17 +35,3 @@ export default function Dashboard() {
     </StyledFeedContainer>
   );
 }
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    margin: theme.spacing(1, 1),
-  },
-  grid: {
-    margin: theme.spacing(2, 1),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    // color: theme.palette.text.secondary,
-  },
-}));
--- src/pages/SinglePing.js
+++ src/pages/SinglePing.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
 import { useParams } from "react-router-dom";
 
 import StyledFeedContainer from "../components/Styled/StyledFeedContainer";
@@ -10,40 +8,16 @@ import RightButtons from "../components/FloatingButtons/RightButtons";
 import Ping from "../components/SinglePing/Ping";
 
 import { useQuery } from "@apollo/client";
-import { useMapContext } from "../utils/useMapContext";
-import {
-  FETCH_PING_QUERY,
-  FETCH_PINGS_BY_LOCATION,
-  NEW_COMMENT_SUBSCRIPTION,
-} from "../utils/graphql";
+import { FETCH_PING_QUERY, NEW_COMMENT_SUBSCRIPTION } from "../utils/graphql";
 
 export default function SinglePing() {
   const [open, setOpen] = useState(false);
-  const classes = useStyles();
   const { pingId } = useParams();
-  const {
-    state: { userPosition },
-    dispatch,
-  } = useMapContext();
   const { subscribeToMore, data, error } = useQuery(FETCH_PING_QUERY, {
     variables: { pingId },
   });
 
-  const pingsData = useQuery(FETCH_PINGS_BY_LOCATION, {
-    skip: !userPosition,
-    variables: { long: userPosition?.longitude, latt: userPosition?.latitude },
-  });
-
   useEffect(() => {
-    const longitude = data && data.getPing.location.coordinates[0];
-    const latitude = data && data.getPing.location.coordinates[1];
-
-    if (longitude && latitude) {
-      dispatch({
-        type: "UPDATE_VIEWPORT",
-        payload: { latitude, longitude, zoom: 13 },
-      });
-    }
     const unsubscribe = subscribeToMore
       ? subscribeToMore({
           document: NEW_COMMENT_SUBSCRIPTION,
@@ -60,27 +34,14 @@ export default function SinglePing() {
     if (unsubscribe) {
       return () => unsubscribe();
     }
-  }, [subscribeToMore, pingId, data, dispatch]);
+  }, [subscribeToMore, pingId, data]);
 
   return (
     <StyledFeedContainer>
       <LeftButtons />
       <RightButtons open={open} setOpen={setOpen} />
       <NewPing open={open} setOpen={setOpen} />
+      <Ping data={data} error={error} />
     </StyledFeedContainer>
   );
 }
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    margin: theme.spacing(1, 1),
-  },
-  grid: {
-    margin: theme.spacing(2, 1),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    // color: theme.palette.text.secondary,
-  },
-}));
