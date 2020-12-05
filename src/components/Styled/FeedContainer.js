03ef3cf97b0f3edfdc2155324399883c06e8423b
Kevin Luo <kvn.luo@gmail.com>
2020-12-05 12:10:31 -0500

    add feed container styled-component

---
+++ src/components/Styled/FeedContainer.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  width: 95vw;
+  margin: 0 auto;
+  padding: 1rem;
+  padding-top: 7rem;
+  padding-bottom: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default StyledDiv;
